015be8266da2a2016344e06f7c1caf353cfff245
flip615 <gfmacaraeg@gmail.com>
2019-01-12 14:40:08 -0600

    added prettier and eslint config

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
-
+import React, { Component } from "react";
+import classes from "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   state = {
     persons: [
-      { id: 'asfa1', name: 'GianFranco', age: 28 },
-      { id: 'vasdf1', name: 'Chinee', age: 29 },
-      { id: 'asdf11', name: 'Jasmine', age: 1 }
+      { id: "asfa1", name: "GianFranco", age: 28 },
+      { id: "vasdf1", name: "Chinee", age: 29 },
+      { id: "asdf11", name: "Jasmine", age: 1 }
     ],
-    otherState: 'some other value',
+    otherState: "some other value",
     showPersons: false
-  }
+  };
 
-  nameChangedHandler = ( event, id ) => {
+  nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
     });
@@ -28,76 +27,66 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState( {persons: persons} );
-  }
+    this.setState({ persons: persons });
+  };
 
-  deletePersonHandler = (personIndex) => {
+  deletePersonHandler = personIndex => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    this.setState({persons: persons});
-  }
+    this.setState({ persons: persons });
+  };
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState( { showPersons: !doesShow } );
-  }
+    this.setState({ showPersons: !doesShow });
+  };
 
-  render () {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      
-    };
-
+  render() {
+    let btnClass = "";
     let persons = null;
 
-    if ( this.state.showPersons ) {
+    if (this.state.showPersons) {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name} 
-              age={person.age}
-              key={person.id}
-              changed={(event) => {
-                this.nameChangedHandler(event, person.id)} 
-              }
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={event => {
+                  this.nameChangedHandler(event, person.id);
+                }}
               />
-              
+            );
           })}
         </div>
       );
-      style.backgroundColor = "red";
-      
+      btnClass = classes.Red;
     }
 
-    let classes = [];
-    if(this.state.persons.length <=2){
-      classes.push('red');
+    let assignedClasses = [];
+    if (this.state.persons.length <= 2) {
+      assignedClasses.push(classes.red);
     }
-    if(this.state.persons.length <= 1){
-      classes.push('bold');
+    if (this.state.persons.length <= 1) {
+      assignedClasses.push(classes.bold);
     }
-    
+
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <p className={assignedClasses.join(" ")}>This is really working!</p>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </button>
         {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
-};
+}
 
 export default App;
-
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import './Person.css';
-import Radium from 'radium';
+import React from "react";
+import classes from "./Person.css";
 
-const person = (props) => {
-    const style = {
-        '@nedia(min-width: 500px': {
-            width: '450px'
-        }
-    };
-    return (
-        <div className="Person" style={style}>
-        <p onClick={props.click}>I'm {props.name} and I am {props.age} years old</p>
-        <p>{props.children}</p>
-        <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
-    
-    )
+const person = props => {
+  return (
+    <div className={classes.Person}>
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
 };
 
-export default Radium(person);
+export default person;
